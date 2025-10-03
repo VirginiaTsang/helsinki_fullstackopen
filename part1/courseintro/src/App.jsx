1b12fbe8378352b757ce1f8cb2bce3ee9aff3b24
VirginiaTsang <virginiacytsang@gmail.com>
2025-10-03 12:31:15 +0800

    1.7: unicafe step 2

--- part1/courseintro/src/App.jsx
+++ part1/courseintro/src/App.jsx
@@ -1,5 +1,18 @@
 import { useState } from 'react'
 
+//defining all, average and positive as components. they can also be as state hooks
+const All = (props) => (
+  <p>all {props.good+props.neutral+props.bad}</p>
+)
+
+const Average = (props) =>(
+  <p>average {(props.good-props.bad)/(props.good+props.neutral+props.bad)}</p>
+)
+
+const Positive = (props) => (
+  <p>positive {(props.good)/(props.good+props.neutral+props.bad)*100} %</p>
+)
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -16,6 +29,11 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <All good={good} neutral={neutral} bad={bad}/>
+      <Average good={good} neutral={neutral} bad={bad}/>
+      <Positive good={good} neutral={neutral} bad={bad}/>
+
+      
     </div>
   )
 }
