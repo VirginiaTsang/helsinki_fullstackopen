ff5ce39c2e4a01da796f7e922e01d9675c3d41c6
VirginiaTsang <virginiacytsang@gmail.com>
2025-10-03 14:17:54 +0800

    1.11: unicafe step 6

--- part1/courseintro/src/App.jsx
+++ part1/courseintro/src/App.jsx
@@ -4,7 +4,12 @@ const Button = (props) => {
   return(<button onClick={props.onClick}>{props.text}</button>)
 }
 const StatisticLine = (props) =>{
-  return (<p>{props.text} {props.value}</p>)
+  return (
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
+)
 }
 
 const Statistics = (props) => {
@@ -12,19 +17,29 @@ const Statistics = (props) => {
   const average = (props.good-props.bad)/(total)
   const positive = `${(props.good)/(total)*100} %`
 
-  if (total===0){
-    return (<StatisticLine text="No feedback given"/>)
-  }
+ if (total === 0) {
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>No feedback given</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
   else{
     return(
-      <div>
-        <StatisticLine text="good" value ={props.good} />
-        <StatisticLine text="neutral" value ={props.neutral} />
-        <StatisticLine text="bad" value ={props.bad} />
-        <StatisticLine text="all" value ={total} />
-        <StatisticLine text="average" value ={average} />
-        <StatisticLine text="positive" value ={positive} />
-      </div>
+      <table>
+        <tbody>
+          <StatisticLine text="good" value ={props.good} />
+          <StatisticLine text="neutral" value ={props.neutral} />
+          <StatisticLine text="bad" value ={props.bad} />
+          <StatisticLine text="all" value ={total} />
+          <StatisticLine text="average" value ={average} />
+          <StatisticLine text="positive" value ={positive} />
+        </tbody>
+      </table>
     )
   }
 }
@@ -36,14 +51,14 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      <h2>give feebback</h2>
+    <>
+      <h2>give feedback</h2>
       <Button onClick={()=>setGood(good+1)} text="good"/>
       <Button onClick={()=>setNeutral(neutral+1)} text="neutral"/>
       <Button onClick={()=>setBad(bad+1)} text="bad"/>
       <h2>statistics</h2>
       <Statistics good={good} neutral={neutral} bad={bad}/>      
-    </div>
+    </>
   )
 }
 
