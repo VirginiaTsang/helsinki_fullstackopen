5cc80288c66e00fc712fc76c5fbc6d3d6acc1d02
VirginiaTsang <virginiacytsang@gmail.com>
2025-10-05 20:30:33 +0800

    2.1: Course information step 6

--- part2/courseintro/src/Content.jsx
+++ part2/courseintro/src/Content.jsx
@@ -1,10 +1,11 @@
 import Part from './Part'
+
 const Content =(props) =>{
+    console.log(props.parts)
     return(
         <div>
-            <Part part={props.course.parts[0].name} exercises={props.course.parts[0].exercises} />
-            <Part part={props.course.parts[1].name} exercises={props.course.parts[1].exercises} />
-            <Part part={props.course.parts[2].name} exercises={props.course.parts[2].exercises} />
+            {props.parts.map(part =>
+                <Part key={part.id} name={part.name} exercises={part.exercises} />)}
         </div>
     )
 }
